d5469b6f9a451b40fea326f91775f023c9608c3c
Erik <erikpastorrios1994@gmail.com>
2025-08-13 08:20:35 +0200

    fix: make heart icon filled red while keeping button text white

--- apps/web/src/features/routines/features/routine-list/components/RoutinesList.tsx
+++ apps/web/src/features/routines/features/routine-list/components/RoutinesList.tsx
@@ -147,7 +147,7 @@ const RoutinesList = ({
             onClick={() => setShowFavoritesOnly(!showFavoritesOnly)}
             className="flex items-center gap-2"
           >
-            <Heart className="w-4 h-4 text-red-500" />
+            <Heart className="w-4 h-4 text-red-500 fill-red-500" />
             Favorites
           </Button>
           <Select value={levelFilter} onValueChange={setLevelFilter}>
