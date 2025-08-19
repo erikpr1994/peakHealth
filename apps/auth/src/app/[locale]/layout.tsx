2aa409608e2ef6014621f364b4e27d673490ccbf
Erik <erikpastorrios1994@gmail.com>
2025-08-19 22:21:47 +0200

    style: remove extra whitespace in layout.tsx

--- apps/auth/src/app/[locale]/layout.tsx
+++ apps/auth/src/app/[locale]/layout.tsx
@@ -21,12 +21,12 @@ export async function generateMetadata({
   params: Promise<{ locale: string }>;
 }): Promise<Metadata> {
   const { locale } = await params;
-  
+
   // Validate locale and redirect to default if invalid
   if (!hasLocale(routing.locales, locale)) {
     redirect(`/${routing.defaultLocale}`);
   }
-  
+
   try {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     const messages = require(`../../../locales/${locale}/index.json`);
@@ -52,7 +52,7 @@ const LocaleLayout = async ({
 }): Promise<React.JSX.Element> => {
   // Ensure that the incoming `locale` is valid
   const { locale } = await params;
-  
+
   // Validate locale and redirect to default if invalid
   if (!hasLocale(routing.locales, locale)) {
     redirect(`/${routing.defaultLocale}`);
